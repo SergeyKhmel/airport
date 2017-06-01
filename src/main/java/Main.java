de087b66d71728ae0546dd22ebfa8142181b7f5f
SergeyKhmel <telec159>
2017-06-01 15:58:54 +0300

    create connection

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Serg_Smile on 30.05.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        DBConnector dbConnector = new DBConnector();
+        dbConnector.createConnection();
+    }
+}
